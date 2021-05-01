49c9adef03b523637d77c2564eac36b945babf49
rehoosa <iuhoosa@gmail.com>
2021-05-01 11:42:43 +0900

    Add: init mysql2

--- NodeAPI/restAPI/index.js
+++ NodeAPI/restAPI/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(port, function () {
     console.log('server on');
 });
