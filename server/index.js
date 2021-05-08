993049665811c6a14edb12892a1a256b4029870c
rehoosa <iuhoosa@gmail.com>
2021-05-08 16:43:41 +0900

    Fix: Change Server Port Number

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
