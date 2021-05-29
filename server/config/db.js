149b345f8ed04c8005d8530313165277fe2306a4
psj98 <park98sj1@hanmail.net>
2021-05-29 17:05:57 +0900

    게시판 수정

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: 'sa981227',
     database: 'project'
   });
   db.connect();
--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 3001;
+//const body = require('body-parser');
+const PORT = process.env.PORT || 3302;
 const db = require('./config/db');
+// const { response, urlencoded } = require('express');
 
-app.use(cors());
-app.use(express.urlencoded());
 app.use(express.json());
+app.use(express.urlencoded({ extend: true }));
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server On : http://localhost:${PORT}/`);
+});
 
 app.get('/hello', (req, res) => {
     res.send({ hello: 'asdf' });
@@ -24,13 +30,13 @@ app.get('/data', (req, res) => {
             throw error;
         }
         res.send(topics);
-        console.log(topics);
+        //console.log(topics);
     });
     console.log("Get datas")
 });
 
 app.get('/data/:id', (req, res) => {
-    db.query(`SELECT * FROM topic WHERE id=?`,[req.params.id] ,function (error, topic) {
+    db.query(`SELECT * FROM topic WHERE id=?`, [req.params.id], function (error, topic) {
         if (error) {
             throw error;
         }
@@ -40,32 +46,37 @@ app.get('/data/:id', (req, res) => {
     });
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
-
 app.post('/data', (req, res) => {
-    console.log(req.body[1]);
-    res.send(req.body[1]);
-    // var id = req.data.id;
-    // var title = req.data.title;
-    // var content = req.data.content;
-    // console.log(req.query,'data posted');
     console.log(req.body.id, "req");
     const d = {
-        id:req.body.id,
-        title:req.body.title,
-        content:req.body.content
+        id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
     }
     res.send(d);
-    // res.send("{ id : " + id + "\ntitle : " + title + "\ncontent : " + content+ " }" );
-    // console.log(res, "res");
+
+    if (req.body.title != null) {
+        db.query(`INSERT INTO topic(id, title, content) VALUES (?,?,?)`, [req.body.id, req.body.title, req.body.content], function (error, result) {
+            if (error) {
+                throw error;
+            }
+            res.end;
+        });
+    }
+
+    else {
+        db.query(`DELETE FROM topic WHERE id = ?`, [req.body.id], function (error, result) {
+            if (error) {
+                throw error;
+            }
+            res.end;
+        });
+    }
 });
 
-// app.post('/data/update/:id/:title/:content', (req, res) => {
-//     db.query(`INSERT INTO topic(id, title, content) VALUES (?,?,?)`,[req.params.id,req.params.title,req.params.content],function(error, result){
-//         if(error){
+// app.post('/data/create/:id/:title/:content', (req, res) => {
+//     db.query(`INSERT INTO topic(id, title, content) VALUES (?,?,?)`, [req.params.id, req.params.title, req.params.content], function (error, result) {
+//         if (error) {
 //             throw error;
 //         }
 //         // res.writeHead(302, {Location:`/data/${res.insertId}`});
@@ -73,18 +84,12 @@ app.post('/data', (req, res) => {
 //     });
 // });
 
-app.delete('/data/delete/:id', (req, res) => {
-    db.query(`DELETE FROM topic WHERE id = ?`,[req.params.id],function(error, result){
-        if(error){
-            throw error;
-        }
-        // res.writeHead(302, {Location:`/data/${res.insertId}`});
-        res.end;
-    });
-});
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server On : http://localhost:${PORT}/`);
-});
+// app.delete('/data/delete/:id', (req, res) => {
+//     db.query(`DELETE FROM topic WHERE id = ?`, [req.params.id], function (error, result) {
+//         if (error) {
+//             throw error;
+//         }
+//         // res.writeHead(302, {Location:`/data/${res.insertId}`});
+//         res.end;
+//     });
+// });
